3ad0b48e5409c67953fb78296c04aff63ad505b2
ssb000ss <ssb000ss@gmail.com>
2017-06-23 15:40:58 +0600

    Был переделан объект Question

--- core/src/main/java/com/gmail/ssb000ss/objects/Question.java
+++ core/src/main/java/com/gmail/ssb000ss/objects/Question.java
@@ -3,7 +3,9 @@ package com.gmail.ssb000ss.objects;
 import com.gmail.ssb000ss.dao.WordList;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by ssb000ss on 21.06.2017.
@@ -11,45 +13,40 @@ import java.util.List;
 
 public class Question {
 
-    private long id;
-    private long correctWordId;
-    private List<Long> incorrectWords = new ArrayList<>(3);
+    //объект вопрос не должен состоять из списка просто ид, а он должен состоять
+    // из спиcка объектов слов и правильного ответа на слово
+    private long idQuestion;
+    private Word correctWord;
+    //как раз для универсальности не ограничиваем длину Set(можно добалять сколько хочешь вариантов)
+    private Set<Word> items = new HashSet<>();
     private Answer answer = null;
 
 
-    public Question(long id, long correctWord, List<Long> incorrectWords) {
-        this.id = id;
-        this.correctWordId = correctWord;
-        this.incorrectWords = incorrectWords;
-    }
-
-    public Question(long correctWord, List<Long> incorrectWords) {
-        this.correctWordId = correctWord;
-        this.incorrectWords = incorrectWords;
+    //на вход подаём ид правельного ответа, и для упрощения список ответов
+    public Question(long id, Word correctWord, List<Word> listWord) {
+        this.idQuestion = id;
+        this.correctWord = correctWord;
+        setItems(listWord);
     }
 
     public long getCorrectWordId() {
-        return correctWordId;
+        return correctWord.getId();
     }
 
-    public void setCorrectWord(long correctWord) {
-        this.correctWordId = correctWord;
-    }
-
-    public List<Long> getIncorrectWords() {
-        return incorrectWords;
-    }
-
-    public void setIncorrectWords(List<Long> incorrectWords) {
-        this.incorrectWords = incorrectWords;
+    public void setCorrectWord(Word correctWord) {
+        this.correctWord = correctWord;
     }
 
     public Answer getAnswer(long w) {
         if (answer != null) {
             return answer;
         } else {
-            if (w == correctWordId) return answer = new Answer(correctWordId, true);
-            else return answer = new Answer(correctWordId, true);
+            if (w == correctWord.getId()) return answer = new Answer(correctWord.getId(), true);
+            else return answer = new Answer(correctWord.getId(), true);
         }
+    }
+
+    public void setItems(List<Word> list) {
+        items.addAll(list);
     }
 }
